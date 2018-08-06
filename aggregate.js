fd872f06a0369a1966101cc17933a9257f607691
Raniere_CC <raniere.souza.cc@gmail.com>
2018-08-06 15:58:07 -0300

    trying to use correctly the aggretateCursor (currently failing, hanging out on collection.aggregate method)

--- aggregate.js
+++ aggregate.js
@@ -1,3 +1,5 @@
+Future = Npm.require('fibers/future');
+
 ReactiveAggregate = function (sub, collection, pipeline, options) {
   var defaultOptions = {
     observeCollections: collection,
@@ -11,17 +13,66 @@ ReactiveAggregate = function (sub, collection, pipeline, options) {
   sub._ids = {};
   sub._iteration = 1;
 
+  //console.log('collection: ', collection);
+  //console.log('collection.aggregate: ', collection.aggregate(pipeline, {cursor: {}}));
+
   function update() {
+
     if (initializing) return;
+    console.log('update sub');
+
+    var fut = new Future();
+    console.log('declared new Future');
+
     // add and update documents on the client
-    collection.aggregate(pipeline, {explain: false}).forEach(function (doc) { // forcing explain option to aggregate
+    var aggregateCursor = collection.aggregate(pipeline, {cursor: {batchSize: 1}}); // forcing cursor option to aggregate
+    console.log('got aggregate cursor');
+
+    aggregateCursor.toArray(function (error, aggregateArray) {
+
+      if (error) {
+        throw 'error from ReactiveAggregate: ' + error.message;
+        fut.throw(error);
+      }
+
+      console.log('got aggregate array');
+      console.log(aggregateArray);
+
+      aggregateArray.forEach(function (doc) { 
+        console.log(doc);
+        if (!sub._ids[doc._id]) {
+          sub.added(options.clientCollection, doc._id, doc);
+        } else {
+          sub.changed(options.clientCollection, doc._id, doc);
+        }
+        sub._ids[doc._id] = sub._iteration;
+      });
+
+      fut['return'](true);
+    });
+
+    fut.wait();
+
+    /*.forEach(function (doc) { // forcing cursor option to aggregate
+      console.log('iteratorCallback');
+      console.log(doc);
       if (!sub._ids[doc._id]) {
         sub.added(options.clientCollection, doc._id, doc);
       } else {
         sub.changed(options.clientCollection, doc._id, doc);
       }
       sub._ids[doc._id] = sub._iteration;
-    });
+    }, function (error) {
+      console.log('endCallback');
+      if (error) {
+        throw 'error from ReactiveAggregate: ' + error.message;
+        //fut.throw(error);
+      }
+      else {
+        //fut['return'](true);
+      }
+    });*/
+
     // remove documents not in the result anymore
     _.forEach(sub._ids, function (v, k) {
       if (v != sub._iteration) {
@@ -30,6 +81,8 @@ ReactiveAggregate = function (sub, collection, pipeline, options) {
       }
     });
     sub._iteration++;
+
+    return true;
   }
 
   // track any changes on the collection used for the aggregation
@@ -86,9 +139,14 @@ ReactiveAggregate = function (sub, collection, pipeline, options) {
   // these are skipped using the initializing flag
   initializing = false;
   // send an initial result set to the client
-  update();
-  // mark the subscription as ready
-  sub.ready();
+  if (update()) {
+    // mark the subscription as ready
+    console.log('starter call to update subscription returned');
+    sub.ready();
+  }
+  else {
+    console.log('starter call to update subscription never returned');
+  }
 
   // stop observing the cursor when the client unsubscribes
   sub.onStop(function () {
